292e56362fa07259ab6a2f264bdeebf6595ffe8f
Nic Lake <niclake13@gmail.com>
2024-08-30 10:37:01 -0500

    Lefty candybar firmware - does not work

---
+++ keyboards/tkc/candybar/lefty/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x4F, 0x0B, 0x1E, 0x8D, 0x0E, 0xB6, 0x3C}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+#define TAPPING_TOGGLE 2
+#define VIAL_COMBO_ENTRIES 24
+#define TAPPING_TERM 150
+#define COMBO_TERM 30
---
+++ keyboards/tkc/candybar/lefty/keymaps/vial/rules.mk
@@ -0,0 +1,8 @@
+COMMAND_ENABLE      = no
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+QMK_SETTINGS        = no
+TAP_DANCE_ENABLE    = no
+KEY_OVERRIDE_ENABLE = no
+COMBO_ENABLE		= yes
